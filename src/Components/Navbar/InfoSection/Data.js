e3a4774c23fc64b303d598f36ebf9feab57f1b2b
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-01-27 23:31:27 +0530

    Started adding Images

---
+++ src/Components/Navbar/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDescription: true,
+    topLine: 'Premium',
+    HeadLine: 'Get Access',
+    description: 'App that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'get Started',
+    imgStart: false;
+    img: require('../../../Images/svg-1.svg');
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false;
+}
